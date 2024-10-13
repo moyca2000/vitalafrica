9f15ddf1ab4af10d11660a8c63e6d432cc9c6a60
JH45 <jonathan@hupressus.com>
2024-10-12 22:59:01 -0600

    Initial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  target: 'static',
-  buildAssetsDir : '',
+  router: {
+    base: '/vitalafrica/',  // Cambia por el nombre de tu repositorio en GitHub
+  },
+  build: {
+    filenames: {
+      img: '[path][name].[ext]',
+    },
+  },
+  generate:{
+    nojekyll: true, //not working on this version
+    fallback: '404.html',
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss']
 })
